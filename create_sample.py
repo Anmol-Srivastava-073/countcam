80b9079a9c3a0c5bcc9b292fbf97f24448b8de40
ANMOL SRIVASTAVA <anmolsrivastava073@gmail.com>
2026-01-19 16:05:44 +0530

    Delete create_sample.py

--- create_sample.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import cv2
-
-img = np.ones((200,200), dtype=np.uint8) * 255
-cv2.putText(img, "5", (60,150), cv2.FONT_HERSHEY_SIMPLEX, 4, (0), 12)
-cv2.imwrite("samples/test1.png", img)
